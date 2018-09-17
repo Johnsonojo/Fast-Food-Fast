b654a2519c5709625ddefe7ac91b16e350001cf7
Johnson Ojo <johnsonojo89@gmail.com>
2018-09-17 12:15:17 +0100

    commit server setup files

---
+++ server/app.js
@@ -0,0 +1,29 @@
+// importing express and other modules
+import express from 'express';
+import logger from 'volleyball';
+import bodyParser from 'body-parser';
+import validation from 'express-validator';
+
+// Setup the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger);
+
+// body-parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.status(200).json({
+    status: 'success',
+    message: 'Welcome to Fast-Food-Fast, a platform to order for food',
+}));
+
+
+app.use(validation());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Fast-Food-Fast is listening on ${PORT}`));
+
+export default app;
