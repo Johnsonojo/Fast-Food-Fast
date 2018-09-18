1781ded16ace24b72776c9673825d66a4c1c505e
Johnson Ojo <johnsonojo89@gmail.com>
2018-09-18 15:20:30 +0100

    api endpoint to post an order

--- server/dummyApi/model/users.js
+++ server/dummyApi/model/users.js
@@ -2,34 +2,20 @@ const users = [{
     id: 1,
     username: 'Adesewa',
     email: 'adesewa@gmail.com',
+    deliveryAddress: '235, Ikorodu road Anthony',
+    phoneNumber: 2348023673478,
 }, {
     id: 2,
     username: 'Johnson',
     email: 'johnson@gmail.com',
+    deliveryAddress: '234, Ikorodu road Anthony',
+    phoneNumber: 2348028923478,
 }, {
     id: 3,
     username: 'Lovetta',
     email: 'lovetta@gmail.com',
-}, {
-    id: 4,
-    username: 'Oluchi',
-    email: 'oluchi@gmail.com',
-}, {
-    id: 5,
-    username: 'Oluchi',
-    email: 'oluchi@gmail.com',
-}, {
-    id: 6,
-    username: 'Oluchi',
-    email: 'oluchi@gmail.com',
-}, {
-    id: 7,
-    username: 'Oluchi',
-    email: 'oluchi@gmail.com',
-}, {
-    id: 8,
-    username: 'Oluchi',
-    email: 'oluchi@gmail.com',
-}];
+    deliveryAddress: '236, Ikorodu road Anthony',
+    phoneNumber: 2348023323478,
+}, ];
 
 export default users;
