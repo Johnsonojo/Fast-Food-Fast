ddc0c81e6596ec5b043e43b835de7b94fb1c1856
Johnson Ojo <johnsonojo89@gmail.com>
2018-09-21 05:01:55 +0100

    fixed bugs on files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 // importing express and other modules
 import express from 'express';
-// import logger from 'volleyball';
+import logger from 'volleyball';
 import bodyParser from 'body-parser';
 import validation from 'express-validator';
 
@@ -11,16 +11,16 @@ import routecontroller from './dummyApi/route/index';
 const app = express();
 
 // Log requests to the console.
-// app.use(logger);
+app.use(logger);
 
 // body-parser configuration
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => res.status(200).json({
-//     status: 'success',
-//     message: 'Welcome to Fast-Food-Fast, a platform to order for food',
-// }));
+app.get('/', (req, res) => res.status(200).json({
+    status: 'Success',
+    message: 'Welcome to Fast-Food-Fast, a platform to order for food',
+}));
 
 
 app.use(validation());
