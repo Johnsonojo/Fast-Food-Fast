51234c73b218454064285802c450a0e0179096ef
Johnson Ojo <johnsonojo89@gmail.com>
2018-10-15 23:10:36 +0100

    Commit put an order endpoint

---
+++ server/tests/__mocks__/statusData.js
@@ -0,0 +1,19 @@
+const status = {
+    processing: {
+        orderStatus: 'Processing'
+    },
+    neew: {
+        orderStatus: 'New'
+    },
+    cancelled: {
+        orderStatus: 'Cancelled'
+    },
+    complete: {
+        orderStatus: 'Complete'
+    },
+    wrong: {
+        orderStatus: 'kljhgfdfcvbnm'
+    }
+};
+
+export default status;
