b52b341248e16781c42d3ff39bc9bc2dd0f26168
Johnson Ojo <johnsonojo89@gmail.com>
2018-10-29 04:24:28 +0100

    Commit signup functionality

--- server/models/menu.js
+++ server/models/menu.js
@@ -1,11 +1,17 @@
 const menuSeed = `
 INSERT INTO menu(foodName,foodPrice,foodImage)
-VALUES ('yamarita', 900, 'www.google.com');
+VALUES ('Yamarita', 900, 'https://bit.ly/2z2Hm21');
 INSERT INTO menu(foodName,foodPrice,foodImage)
-VALUES ('meat pie', 300, 'www.google.com');
+VALUES ('Fried Rice', 800, 'https://bit.ly/2qbYUEN');
 INSERT INTO menu(foodName,foodPrice,foodImage)
-VALUES ('beef shawarma', 800, 'www.google.com');
+VALUES ('Meat Pie', 200, 'https://bit.ly/2yzDczo');
 INSERT INTO menu(foodName,foodPrice,foodImage)
-VALUES ('jollof rice', 1200, 'www.google.com');`;
+VALUES ('Glazed Donought', 150, 'https://bit.ly/2CCi0vr');
+INSERT INTO menu(foodName,foodPrice,foodImage)
+VALUES ('Chicken and Chips', 800, 'https://bit.ly/2ENhrlf');INSERT INTO menu(foodName,foodPrice,foodImage)
+VALUES ('Beef Hamburger', 600, 'https://bit.ly/2yBPzL6');INSERT INTO menu(foodName,foodPrice,foodImage)
+VALUES ('Beef Shawarma', 500, 'https://bit.ly/2D4YlVJ');INSERT INTO menu(foodName,foodPrice,foodImage)
+VALUES ('Chicken Shawarma', 700, 'https://bit.ly/2CH7cw7');INSERT INTO menu(foodName,foodPrice,foodImage)
+VALUES ('Jollof Rice', 900, 'https://bit.ly/2D68gun');`;
 
 export default menuSeed;
